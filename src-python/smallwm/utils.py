a01c7a66423420e69cdb035031f3f277b166565c
adamnew123456 <adamnew123456@gmail.com>
2014-06-04 19:12:54 -0400

    Wrote the desktop management functions.

--- src-python/smallwm/utils.py
+++ src-python/smallwm/utils.py
@@ -8,6 +8,14 @@ SmallWM.
 #    Dialogs are [110, 119]
 MAX_LAYER = 119
 
+# The properties that are stored for each client
+(CD_DESKTOP, # The current desktop of the client
+ CD_STICKY, # Whether the client is sticky or not
+ CD_LAYER, # The current stacking layer of the client
+ CD_STATE, # The currents state of the client
+ CD_ICON, # The icon state of the clientA
+) = range(5)
+
 def positive_int(text):
     """
     Converts a str into a positive int - if the str is not a valid number, or
@@ -17,3 +25,9 @@ def positive_int(text):
     if value <= 0:
         raise ValueError
     return value
+
+def is_visible(wm, window):
+    """
+    Determines whether or not a window should be visible.
+    """
+    raise NotImplementedError
