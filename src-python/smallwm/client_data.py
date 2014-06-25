011881fd369be593d98a3e97ec40a7177e142af8
adamnew123456 <adamnew123456@gmail.com>
2014-06-25 13:52:29 -0400

    Wrote the layer handling methods.

--- src-python/smallwm/client_data.py
+++ src-python/smallwm/client_data.py
@@ -178,3 +178,75 @@ class ClientData:
 
         self.location[client] = (geometry.x, geometry.y)
         self.size[client] = (geometry.width, geometry.height)
+
+    def find_desktop(self, client):
+        """
+        Finds the desktop which is inhabited by a client.
+
+        :param client: The client to find.
+        :return: The desktop that the client is on.
+        :raises KeyError: If the client isn't listed in the desktop list.
+        """
+        for desktop, win_list in self.desktops.items():
+            if client in win_list:
+                return desktop
+
+        raise KeyError('Could not find the desktop of a client')
+
+    def find_layer(self, client):
+        """
+        Finds the layer which is inhabited by a client.
+
+        :param client: The client to find.
+        :return: The layer that the client is on.
+        :raises KeyError: If the client isn't listed in the layer list.
+        """
+        for layer, win_list in self.layers.items():
+            if client in win_list:
+                return layer
+
+        raise KeyError('Could not find the layer of a client')
+
+    def up_layer(self, client):
+        """
+        Moves a client up one layer.
+
+        :param client: The client window to move up.
+        """
+        old_layer = self.find_layer(client)
+        if old_layer < utils.MAX_LAYER:
+            self.push_change(ChangeLayer(client))
+
+            self.layers[old_layer].remove(client)
+            self.layers[old_layer + 1].add(client)
+
+    def down_layer(self, client):
+        """
+        Moves a client down one layer.
+
+        :param client: The client window to move down.
+        """
+        old_layer = self.find_layer(client)
+        if old_layer > utils.MIN_LAYER:
+            self.push_change(ChangeLayer(client))
+
+            self.layers[old_layer].remove(client)
+            self.layers[old_layer - 1].add(client)
+
+    def set_layer(self, client, layer):
+        """
+        Sets the layer of a client.
+
+        :param client: The client to set the layer of.
+        :param int layer: The layer to set the client to.
+        :raises ValueError: If the given layer is an invalid layer.
+        """
+        if layer not in self.layers:
+            raise ValueError('The layer {} is not a valid layer'.format(repl(layer)))
+
+        old_layer = self.find_layer(client)
+        if old_layer != layer:
+            self.push_change(ChangeLayer(client))
+
+            self.layers[old_layer].remove(client)
+            self.layers[layer].add(client)
